e2e059d9656a9a3b94672b6860d75c45b85655c1
KSIMAN <somnia.universum@gmail.com>
2023-04-29 14:25:58 +0300

    new

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
+from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
 from scipy.spatial import distance
 from skimage import io
-import imageio
-from skimage.feature import blob_dog, blob_log, blob_doh
 from skimage.color import rgb2gray
-from sympy import  Line2D
-import matplotlib.pyplot as plt
+from skimage.feature import blob_log
 
-from skimage.segmentation import slic, mark_boundaries
+from skimage.segmentation import felzenszwalb, mark_boundaries
 import numpy as np
 from sklearn.cluster import KMeans
 
-
-#image = io.imread(input("Путь до изображения: "))
-
-image = imageio.imread("photos/2.jpg")[:,:,:3]
-
-print(image.shape)
-
-image_gray = rgb2gray(image)
+white = (255, 255, 255)
 
 
-segments = slic(image_gray, start_label=0, n_segments=200, compactness=20)
+def my_distance(a, b):
+    return distance.euclidean(a, b)
+
+
+img = io.imread("photos/2.jpg")
+image_gray = rgb2gray(img)
+fig = plt.figure()
+ax = fig.add_subplot(1, 2, 1)
+#
+ax.set_title('Image')
+ax.imshow(img)
+ax = fig.add_subplot(1, 2, 2)
+#
+ax.set_title('GrayImage')
+ax.imshow(image_gray)
+
+# SLIC
+segments = felzenszwalb(image_gray, scale=200,  sigma=0.5, min_size=100)
 segments_ids = np.unique(segments)
 print(segments_ids)
 
@@ -34,7 +43,7 @@ bneighbors = np.unique(np.hstack([vs_right, vs_below]), axis=1)
 
 fig = plt.figure(figsize=(10, 10))
 ax = fig.add_subplot(111)
-plt.imshow(mark_boundaries(image_gray, segments))
+plt.imshow(mark_boundaries(img, segments))
 plt.scatter(centers[:, 1], centers[:, 0], c='y')
 
 for i in range(bneighbors.shape[1]):
@@ -44,21 +53,45 @@ for i in range(bneighbors.shape[1]):
     l = Line2D([x0, x1], [y0, y1], alpha=0.5)
     ax.add_line(l)
 
-blobs_log = blob_log(image_gray, max_sigma=20, num_sigma=10, threshold=.05)
+#
+def middle(a, b):
+    color = []
+    for i, j in zip(a, b):
+        color.append((i + j) // 2)
+    return color
 
+
+dict_seg = {}
+for i in range(img.shape[0]):
+    for j in range(img.shape[1]):
+        seg = segments[i, j]
+        if seg not in dict_seg.keys():
+            dict_seg[seg] = 1
+            continue
+        dict_seg[seg] += 1
+max_l = max(dict_seg, key=dict_seg.get)
+print(max_l)
+blobs_log = blob_log(image_gray, max_sigma=100, num_sigma=10, threshold=.09)
 fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-
+ax = fig.add_subplot(1, 2, 1)
+#
 ax.set_title('Laplacian of Gaussian')
-ax.imshow(image)
-c_stars = 0
+ax.imshow(img)
+count = 0
 for blob in blobs_log:
     y, x, r = blob
-    if r > 2:
-        continue
-    ax.add_patch(plt.Circle((x, y), r, color='purple', linewidth=2, fill=False))
-    c_stars += 1
-print("Количество звёзд: " + str(c_stars))
+    # print(max_l, end=" ")
+    # print(dic_seg_claster[segments[int(y), int(x)]])
+    if segments[int(y), int(x)] == max_l:
+        c = plt.Circle((x, y), r, color='white', linewidth=2, fill=False)
+        count += 1
+        ax.add_patch(c)
+print(count)
+ax.set_axis_off()
+ax = fig.add_subplot(1, 2, 2)
+#
+ax.set_title('Оригинал')
+ax.imshow(img)
 ax.set_axis_off()
 plt.tight_layout()
 plt.show()
